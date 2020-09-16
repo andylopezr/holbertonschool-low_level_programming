826e945b0349d48ec6a73504a178248dd6ff6db1
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-16 23:09:10 +0000

    Hotfix1 Task6 file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 
-	char ba;
-	int bb;
-	long bc;
-	double bd;
-	float be;
+	char;
+	int;
+	long;
+	double;
+	float;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(ba));
-	printf("Size of an int: %d byte(s)\n", sizeof(bb));
-	printf("Size of a long int: %d byte(s)\n", sizeof(bc));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(bd));
-	printf("Size of a float: %d byte(s)\n", sizeof(be));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(double));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }

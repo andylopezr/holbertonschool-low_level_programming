6a822f526cb66bf32a6bb639cc70dc869d19b198
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-17 23:22:22 +0000

    Hotfix1 Task1 file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,18 +19,14 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("%d and is greater than 5\n", n % 10);
-		
 	}
 	if ((n % 10) == 0)
 	{
 		printf("%d and is 0\n", n % 10);
-		
 	}
 	if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("%d and is less than 6 and not 0\n", n % 10);
-		
 	}
 	return (0);
-	
 }

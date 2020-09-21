aa47884488fd8695d07638b1b141020e0eaa1d19
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-21 19:20:05 +0000

    Added Task 11 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints single digit numbers
+ *
+ * Return: 0 Always, on completion.
+ */
+int main(void)
+{
+	int num = 48;
+	int num2;
+	while (num <= 57)
+	{
+		for (num2 = 48; num2 <= 57; num2++)
+		{
+			if (num < num2)
+			{
+				putchar(num);
+				putchar(num2);
+				if (num != 56 || num2 != 57)
+				{
+					putchar(44);
+					putchar(32);
+
+				}
+
+			}
+
+		}
+		num++;
+	}
+	putchar(10);
+	return (0);
+}

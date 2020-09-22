6f58db49d499328695a4a2b97c61a80a874adc49
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-22 14:01:16 +0000

    Added Task2 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int alphabet = 'a';
+
+	for (alphabet >= '10')
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+
+	}
+
+
+	_putchar('\n');
+	alphabet++;
+
+}

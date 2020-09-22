5ac69a5764654a2f03090efc0c71b79a42c70504
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-22 13:29:52 +0000

    Added README.md, Task0 file and README.md file

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+** 0x02. C - Functions, nested loops **
+* This dir includes different Tasks containing functions and nested loops. *
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+/**
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+/*writes the character c to stdout */
+int _putchar(char c);
+
+#endif /* _SAMPLE_HEADER_H_ */

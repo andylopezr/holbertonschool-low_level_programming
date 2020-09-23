a9beb8c9c2869304ae54c88d39616657430d2dd9
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-23 22:05:53 +0000

    Hotfix2 holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,1 +1,24 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+#include <stdio.h>
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 int largest_number(int a, int b, int c);
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day);
+
+#endif /* _SAMPLE_HEADER_H_ */

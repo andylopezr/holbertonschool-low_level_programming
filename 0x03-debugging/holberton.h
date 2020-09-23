a8010fbc208d150aced61c38f93f56e9db885494
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-23 23:18:31 +0000

    Hotfix3 holberton.h, Hotfix2 Task 3 file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -21,4 +21,14 @@ int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
 
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
+
+void print_remaining_days(int month, int day, int year);
 #endif /* _SAMPLE_HEADER_H_ */

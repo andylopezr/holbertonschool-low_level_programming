3f3f7cab8b38aa9b40f8f305643686de2ba615f1
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-24 22:55:34 +0000

    Added README.md, holberton.h and Task0 files

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+** C - More functions, more nested loops **
+* nested loops and how to use them *
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*checks for uppercase character*/
+int _isupper(int c);
+
+#endif

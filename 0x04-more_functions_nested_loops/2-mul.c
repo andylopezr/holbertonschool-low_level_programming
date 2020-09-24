e465399ebbd65f7b41ea8484fd868b61802c85c0
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-24 23:16:17 +0000

    Added Task2 file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers.
+ * @a - first integer.
+ * @b - second intiger
+ * Return: result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,7 +3,12 @@
 
 /* checks for uppercase character */
 int _isupper(int c);
+
 /* checks for a digit (0 through 9). */
 int _isdigit(int c);
+
+/* multiplies two integers. */
+int mul(int a, int b);
+
 
 #endif

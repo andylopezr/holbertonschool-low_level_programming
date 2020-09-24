7b7857bf1d46f9d65c9f9e789efc403294859093
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-24 23:00:55 +0000

    Hotfix1 Task0 file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,18 @@
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to test.
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 
 	else
 	{
-		return(0);
+		return (0);
 	}
 }

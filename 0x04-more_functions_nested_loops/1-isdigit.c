5c50a2d1026262685074c1f44575e7f5437b1b47
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-24 23:10:20 +0000

    Added Task1 file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: character to test.
+ * Return: Returns 1 if c is a digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,7 +1,9 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
 
-/*checks for uppercase character*/
+/* checks for uppercase character */
 int _isupper(int c);
+/* checks for a digit (0 through 9). */
+int _isdigit(int c);
 
 #endif

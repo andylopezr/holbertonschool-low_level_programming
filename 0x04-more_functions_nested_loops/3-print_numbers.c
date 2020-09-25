0bd32f7307302795df28f0193e7d7389a1ed33c8
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-25 00:35:36 +0000

    Hotfix Task1 file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_numbers prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  * Return: void.
  */
 

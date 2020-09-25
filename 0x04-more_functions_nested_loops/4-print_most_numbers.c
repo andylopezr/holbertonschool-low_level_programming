611eaab1f4934b303d345794446d944bc75c00dc
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-25 00:55:01 +0000

    Hotfix1 Task4 file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * but 2 and 4followed by a new line.
  * Return: void.
  */
 

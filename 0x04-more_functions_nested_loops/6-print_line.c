a89da33f58573c05180e5f928716b24ed2645989
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-25 02:47:10 +0000

    Added Task6 file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,18 @@
 
 /**
  * print_line - draws a straight line in the terminal.
+ * @n : prints the character _.
  */
 
 void print_line(int n)
 {
 	int contador;
+
 	if (n > 0)
 	{
 		for (contador = 0; contador <= n; contador++)
 		{
-		_putchar('_');
+		_putchar ('_');
 		}
 	}
 	_putchar(10);

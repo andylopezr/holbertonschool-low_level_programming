8cac4283aad9a16f54175cf75478d083133deedd
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-25 00:34:09 +0000

    Added Task3 file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,9 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
 
+/* writes the character c to stdout. */
+int _putchar(char c);
+
 /* checks for uppercase character */
 int _isupper(int c);
 
@@ -10,5 +13,7 @@ int _isdigit(int c);
 /* multiplies two integers. */
 int mul(int a, int b);
 
+/* prints the numbers, from 0 to 9, followed by a new line. */
+void print_numbers(void);
 
 #endif

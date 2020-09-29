807a21cfe15a664ce5ffb006f1bfd6dd20c7a319
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-29 04:16:05 +0000

    Added holberton.h file

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,22 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/* writes the character c to stdout. */
+int _putchar(char c);
+
+/*  takes a pointer to an int as parameter and updates the value it points to to 98. */
+void reset_to_98(int *n);
+
+/* swaps the values of two integers. */
+void swap_int(int *a, int *b);
+
+/* returns the length of a string. */
+int _strlen(char *s);
+
+/* prints a string, followed by a new line, to stdout. */
+void _puts(char *str);
+
+/* prints a string, in reverse, followed by a new line. */
+void print_rev(char *s);
+
+#endif

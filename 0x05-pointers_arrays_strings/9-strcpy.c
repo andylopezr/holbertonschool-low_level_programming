ba0088bc5d27925a0661c98884d0003d2eec214b
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-29 19:35:36 +0000

    Added Task9 file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+# include "holberton.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: array recive
+ * @src: array done
+ * Return: dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

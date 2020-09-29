a58b3651e3cd201183b87505a84523b8aaf9d969
Andy Lopez <profesor.de.ingles@aol.com>
2020-09-29 04:20:23 +0000

    Added Task4 file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s : string received.
+ */
+
+void print_rev(char *s)
+{
+	int c, d;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+	for (d = c; d >= 0; d--)
+	{
+		_putchar(s[d]);
+	}
+
+	_putchar(10);
+
+}

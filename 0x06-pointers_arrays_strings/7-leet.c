6029a02f4ccd22cfcafc25f203c8fd0d139a9e7a
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-02 03:30:03 +0000

    Hotfix0 Task7 file - added return var

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,14 +2,12 @@
 /**
  * leet - encodes a string into 1337
  * @x: string to change
- *
- *
- *
  * Return: char
  */
 char *leet(char *x)
 {
 	int y = 0;
+	char *end = x;
 	char *letters = "aAeEoOtTlL";
 	char *numbers = "4433007711";
 
@@ -24,5 +22,5 @@ char *leet(char *x)
 		}
 		x++;
 	}
-	return (x);
+	return (end);
 }

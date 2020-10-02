7a48b791f341709625710d38cfd0f035247c4fec
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-02 03:54:07 +0000

    Hotfix Task8 file changed *rot13 variable inside for loop

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,20 +7,21 @@
 char *rot13(char *x)
 {
 	int y = 0;
+	char *rot13 = x;
 	char *value1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char *value2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (*x)
+	while (*rot13)
 	{
 		for (y = 0; value1[y]; y++)
 		{
-			if (*x == value1[y])
+			if (*rot13 == value1[y])
 			{
-				*x = value2[y];
+				*rot13 = value2[y];
 				break;
 			}
 		}
-		x++;
+		rot13++;
 	}
 	return (x);
 }

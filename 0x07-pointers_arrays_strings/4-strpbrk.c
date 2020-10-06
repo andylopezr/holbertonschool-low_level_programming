c8855ef3ae2cb945b3a3cb47cbe3353f2d237f90
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-06 04:43:33 +0000

    Hotfix4 Task4 file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * _strpbrk - searches in string for any of a set of bytes.
+ * @s: string to compare from.
+ * @accept: string to compare with.
+ * Return: Pointer to the first match.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, find = 0;
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0 ; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				find++;
+				break;
+			}
+		}
+		if (find == 1)
+		{
+			break;
+		}
+	}
+	if (find == 1)
+	{
+		p = (s + i);
+		return (p);
+	}
+	else
+	{
+		return ('\0');
+	}
+}

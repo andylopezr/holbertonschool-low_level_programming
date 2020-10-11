43ef0763829fcf353aa5c2e04b911611851f6224
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-11 17:29:01 +0000

    Added Task1 file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ *@argc: number of parameters.
+ *@argv: array of value argc.
+ *Return: Always 0.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

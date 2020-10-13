0629ad1d338a06e70c2d358ebdb539a7155f5a2c
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-13 23:49:47 +0000

    Optimized Task1 code

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,21 @@
 #include "holberton.h"
 /**
  * _strdup - return a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter
+ * which contains a copy of the string given as a parameter.
  * @str: input string
- *
- * Return: NULL if str = NULL otherwise pointer to the new str
+ * Return: Pointer to the new str, NULL if memory unavailable.
  */
-
 char *_strdup(char *str)
 {
-	int counter, b = 0;
+	int counter, b = 1;
 	char *dup;
-
-	if (str == NULL)
-		return (NULL);
 
 	while (*(str + b))
 		b++;
-
 	dup = malloc(b * sizeof(char) + 1);
-	if (dup == NULL)
-		return (NULL);
 	for (counter = 0; counter <= b; counter++)
 		*(dup + counter) = *(str + counter);
+	if (str == NULL || dup == NULL)
+	return (NULL);
 	return (dup);
 }

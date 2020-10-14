e384ade746979e872a18938f94f7d95c879d1d1a
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-14 04:43:26 +0000

    Added Task2 file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the new string, NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *sout;
+	unsigned int i, j, l1 = 0, l2 = 0;
+
+	if (s1 != NULL)
+		while (*(s1 + l1))
+			l1++;
+	if (s2 != NULL)
+		while (*(s2 + l2))
+			l2++;
+	sout = malloc((sizeof(char) * (l1 + l2)) + 1);
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < l1; i++)
+		*(sout + i) = *(s1 + i);
+	for (j = 0; j < l2; j++, i++)
+		*(sout + i) = *(s2 + j);
+	return (sout);
+}

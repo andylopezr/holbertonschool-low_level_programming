3720ace2f8e62ae7b9947f19ea699bd4aeb51a14
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-14 16:01:07 +0000

    Hotfix Task2 - organized code again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,10 +9,17 @@ char *str_concat(char *s1, char *s2)
 {
 	char *sout;
 	unsigned int i, j, l1, l2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (s1 != NULL)
 		for (l1 = 0; *(s1 + l1); l1++)
+			;
 	if (s2 != NULL)
 		for (l2 = 0; *(s2 + l2); l2++)
+			;
 	sout = malloc((sizeof(char) * (l1 + l2)) + 1);
 	if (sout == NULL)
 		return (NULL);

6c91265f670442a5c001cd38e4f5d0438873584f
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-16 04:44:18 +0000

    Updated prototype Task4

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -19,4 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 /* creates an array of integers. */
 int *array_range(int min, int max);
 
+/* reallocates a memory block using malloc and free. */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif

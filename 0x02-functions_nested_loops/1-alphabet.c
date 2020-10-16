d6997f87970c0bb2329f94cfdc3db0971a2b5743
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-16 19:57:21 +0000

    Task1 - updated and optimized code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,14 @@
 #include "holberton.h"
-
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet - prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
-
 void print_alphabet(void)
 {
 	int alphabet = 'a';
 
-	while (alphabet <= 'z')
-	{
+	for (; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
-			alphabet++;
-	}
 	_putchar('\n');
-
 }

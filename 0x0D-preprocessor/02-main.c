cd19e79f8337de57e6cb664f0a4908eb029c2afb
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-19 17:15:32 +0000

    Added Task2 file

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compile from + new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

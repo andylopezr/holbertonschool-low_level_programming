880c38a920bb252d15bc25abd479578de6b644d3
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-19 19:34:06 +0000

    Added Task0 file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTDOG_
+#define STRUCTDOG_
+/**
+ * struct dog - Variables for dog description
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif 

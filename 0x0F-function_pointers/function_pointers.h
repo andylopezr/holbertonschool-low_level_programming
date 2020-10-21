9d905ce088e8e1d177e125e2fe77f46d45b2f746
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-21 18:35:07 +0000

    Included libraries

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,10 @@
 #ifndef _FUNCTIONPOINTERS_H_
 #define _FUNCTIONPOINTERS_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+/* writes characters to stdout. */
+int _putchar(char c);
 
 /* prints a name. */
 void print_name(char *name, void (*f)(char *));

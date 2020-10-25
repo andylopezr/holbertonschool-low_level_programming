fdca6f5067727ef4c5fa8a5f72662c075ec8cab1
Andy Lopez <profesor.de.ingles@aol.com>
2020-10-25 03:37:57 +0000

    changed va_start and if locations

---
+++ 2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: amount of parameters.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *s;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(list, char*);
+		if (s)
+		{
+			printf("%s", s);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(list);
+	printf("\n");
+}

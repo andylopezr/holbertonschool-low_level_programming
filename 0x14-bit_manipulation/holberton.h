e26082b60349f5888c6315a62c3b7710ac8fe039
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-06 01:26:42 +0000

    Added libraries

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,10 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
 int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);

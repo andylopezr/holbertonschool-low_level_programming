932062261d4bea9a8f4dbb225213df7d4eba0063
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-09 20:41:33 +0000

    Added prototypes

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,18 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+/* reads a text file and prints it to the POSIX standard output */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* creates a file */
+int create_file(const char *filename, char *text_content);
+
+/* appends text at the end of a file */
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* _HOLBERTON_H_ */

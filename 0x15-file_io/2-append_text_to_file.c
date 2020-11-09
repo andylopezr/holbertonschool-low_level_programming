328972ef4f255894b40163a5df0a7e778f34632c
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-09 22:13:55 +0000

    Added brackets to if condition

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 	{}
 	if (!filename)
+	{
 		return (-1);
+	}
 	else
 	{
 		fd = open(filename, O_WRONLY | O_APPEND);

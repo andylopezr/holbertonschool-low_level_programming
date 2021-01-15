06fef11f09ef8fa41f3c70d6c94c989035eeab7b
Andy Lopez <profesor.de.ingles@aol.com>
2021-01-15 10:57:41 -0500

    Fixed typo

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	hash_table = malloc(sizeof(hash_table_table_t));
+	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;

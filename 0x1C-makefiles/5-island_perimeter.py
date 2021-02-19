a17352c2735758fe87007ac260cf8f7ca1133993
Andy Lopez <profesor.de.ingles@aol.com>
2021-02-19 15:08:44 -0500

    Updated conditions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,16 +5,16 @@
 def island_perimeter(grid):
     """Function that returns the perimeter of the island described in grid"""
     perimeter = 0
-    for row in range(len(grid)):
-        for position in range(len(grid[row])):
-            if grid[row][position] == 0:
+    for columns in range(len(grid)):
+        for rows in range(len(grid[columns])):
+            if grid[columns][rows] == 0:
                 continue
-            if row == 0 or grid[row - 1][position] == 0:
+            if columns == 0 or grid[columns - 1][rows] == 0:
                 perimeter += 1
-            if row == rows - 1 or grid[row + 1][position] == 0:
+            if columns == rows - 1 or grid[columns + 1][rows] == 0:
                 perimeter += 1
-            if position == 0 or grid[row][position - 1] == 0:
+            if rows == 0 or grid[columns][rows - 1] == 0:
                 perimeter += 1
-            if position == columns - 1 or grid[row][position + 1] == 0:
+            if rows == columns - 1 or grid[columns][rows + 1] == 0:
                 perimeter += 1
     return perimeter
